4d931ac68a9dfb4cbfda5197682d0f60dd2188eb
Matt Descoteaux <matt.descoteaux@outlook.com>
2022-08-23 19:55:40 -0400

    Updates

--- src/Component/Champ.jsx
+++ src/Component/Champ.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 function Champ() {
-	return <div>Champ</div>
+	return (
+		<div className="champContainer">
+			<div className="champName">The Champ</div>
+			<div className="champMessage">WHATS UP</div>
+		</div>
+	)
 }
 
 export default Champ
